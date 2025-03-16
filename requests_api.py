0ecf0f028aed0ec40c49d484d638ac9d5716eb28
Alex <coolrobottt@gmail.com>
2025-03-16 11:00:16 +0300

    Revert "--amend"
    This reverts commit 51b7c24d18d6f67fefcd91ddb0489942a34b7417.

--- requests_api.py
+++ requests_api.py
@@ -6,12 +6,7 @@ class RequestsCollector:
         self.url = r'https://olimp.miet.ru/ppo_it/api'
 
     def get_map(self):
-        maps = []
-        while len(maps) != 16:
-            data = requests.get(self.url).json()['message']['data']
-            if data not in maps:
-                maps.append(data)
-        return maps
+        return requests.get(self.url).json()['message']['data']
 
     def get_coords(self):
         return requests.get(self.url + '/coords').json()['message']
@@ -20,4 +15,4 @@ class RequestsCollector:
 if __name__ == '__main__':
     rc = RequestsCollector()
 
-    print(rc.get_map())
+    print(rc.get_coords())
